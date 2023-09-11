f2fa2d3ec87101c180afdfe5370098f9473dadec
Narender Singh Bisht <narendersinghbisht382@gmail.com>
2023-09-11 12:02:30 +0530

    jsByRoadSideCoder

---
+++ React/Notes.js
@@ -0,0 +1,22 @@
+//*React
+//It is an open-source javascipt libraray for building user Interface (UIs).
+//It is developed by facebook and is widely used for creating interactive and dynamic web applications
+
+//*Component-Based Architecture: 
+//React follows a component-based architecture. 
+//A component is a modular, reusable piece of the user interface. 
+//Components can be thought of as building blocks that can be combined to create complex UIs. 
+
+//*Virtual DOM Concept: React introduces the concept of a Virtual DOM (Document Object Model). 
+//Instead of directly updating the actual browser DOM, React creates a lightweight representation of the DOM in memory. 
+//When changes occur, React calculates the most efficient way to update the Virtual DOM and then applies those changes to the actual DOM, reducing unnecessary re-renders. 
+
+//*JSX (JavaScript XML): 
+//JSX is a syntax extension that allows you to write HTML-like code within JavaScript. 
+//It enables the creation of React components using a syntax that closely resembles HTML. 
+//JSX gets transpiled into regular JavaScript code by tools like Babel. 
+
+//*Rendering Components: 
+//To render a React component, you define its structure and behavior using JSX and JavaScript. 
+//Components can be either functional or class-based. 
+//They receive input data (props) and can maintain their own internal state."
