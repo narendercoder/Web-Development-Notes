1dd8647dec9d42365cad985b0a29ddc121ad59cd
Narender Singh Bisht <narendersinghbisht382@gmail.com>
2024-02-08 22:09:35 +0530

    ( 'this' Keyword ) - Output Based, Scope, Implicit Binding, etc

--- javascript/Javascript Interview Questions(RoadsideCoder)/7_Lacture.js
+++ javascript/Javascript Interview Questions(RoadsideCoder)/7_Lacture.js
@@ -2,9 +2,7 @@
 // Explain 'this' keyword
 
 //In JavaScript, the this keyword refers to an object.
-
 //Which object depends on how this is being invoked (used or called).
-
 //The this keyword refers to different objects depending on how it is used:
 
 /*
